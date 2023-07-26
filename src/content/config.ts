5f018122850fece4ac5c816cf729ac9545c99190
juanpabotero <juanpabotero1@gmail.com>
2023-07-26 11:44:14 -0500

    fix: add optional props

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,12 +1,23 @@
 import { defineCollection, z } from 'astro:content';
 
 const notes = defineCollection({
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		id: z.string(),
-		title: z.string(),
-		heroImage: z.string().optional(),
-	}),
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    id: z.string(),
+    title: z.string(),
+    description: z.string().optional(),
+    // Transform string to Date object
+    pubDate: z
+      .string()
+      .or(z.date())
+      .transform((val) => new Date(val))
+      .optional(),
+    updatedDate: z
+      .string()
+      .optional()
+      .transform((str) => (str ? new Date(str) : undefined)),
+    heroImage: z.string().optional(),
+  }),
 });
 
 export const collections = { notes };
