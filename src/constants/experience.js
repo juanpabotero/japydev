c5185b49241f9037de05f27774a82a8d2489706f
juanpabotero <juanpabotero1@gmail.com>
2023-08-16 16:55:09 -0500

    feat: add experience

---
+++ src/constants/experience.js
@@ -0,0 +1,27 @@
+export const experience = [
+  {
+    title: 'Desarrollador Frontend',
+    company: 'Michael Page',
+    date: 'Feb/2023 - Actualidad',
+    description: [
+      'Apoyo en el equipo de FRONT para las células del Proyecto Portafolio.'
+    ]
+  },
+  {
+    title: 'Desarrollador Frontend',
+    company: 'Universidad Tecnológica de Pereira',
+    date: 'Ago/2022 - Ene/2023',
+    description: [
+      'Desarrollo Frontend usando ANGULAR como Framework para el proyecto "Fortalecimiento en capacidades de salud".'
+    ]
+  },
+  {
+    title: 'Asesor independiente',
+    company: 'Japy Soluciones Digitales',
+    date: 'Ene/2022 - Ago/2022',
+    description: [
+      'Creación de soluciones para empresas y particulares a través del uso de herramientas tecnológicas.',
+      'Asesorar a empresas y particulares para definir requerimientos técnicos.'
+    ]
+  }
+];
