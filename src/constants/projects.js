636e67dfef6392d713d2a7729c4c1619edf1ffd1
juanpabotero <juanpabotero1@gmail.com>
2023-08-16 17:46:01 -0500

    feat: add linter

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -4,34 +4,34 @@ export const projects = [
     img: '/reading-list.webp',
     stack: 'React • Vite • JS • CSS • HTML',
     title: 'Lista de lectura',
-    url: 'https://reading-list-japydev.netlify.app/',
+    url: 'https://reading-list-japydev.netlify.app/'
   },
   {
     id: 2,
     img: '/counter.webp',
     stack: 'React • Vite • JS • CSS • HTML',
     title: 'Contador',
-    url: 'https://juanpabotero.github.io/react-counter/',
+    url: 'https://juanpabotero.github.io/react-counter/'
   },
   {
     id: 3,
     img: '/password-generator.webp',
     stack: 'Angular • TS • CSS • HTML',
     title: 'Generador de contraseñas',
-    url: 'https://juanpaboterodev.netlify.app/password-generator',
+    url: 'https://juanpaboterodev.netlify.app/password-generator'
   },
   {
     id: 4,
     img: '/advice-generator.webp',
     stack: 'Angular • API • TS • CSS • HTML',
     title: 'Generador de consejos',
-    url: 'https://juanpaboterodev.netlify.app/advice-generator',
+    url: 'https://juanpaboterodev.netlify.app/advice-generator'
   },
   {
     id: 5,
     img: '/tip-calculator.webp',
     stack: 'Angular • TS • CSS • HTML',
     title: 'Calculadora de propinas',
-    url: 'https://juanpaboterodev.netlify.app/tip-calculator',
-  },
+    url: 'https://juanpaboterodev.netlify.app/tip-calculator'
+  }
 ];
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,14 +3,14 @@ import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
 export async function get(context) {
-	const posts = await getCollection('notes');
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/notes/${post.slug}/`,
-		})),
-	});
+  const posts = await getCollection('notes');
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    items: posts.map((post) => ({
+      ...post.data,
+      link: `/notes/${post.slug}/`
+    }))
+  });
 }
