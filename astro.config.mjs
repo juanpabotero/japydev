2d19f9cc4dfee2cb6d5dc640416590731c3ba61f
juanpabotero <juanpabotero1@gmail.com>
2023-09-06 18:54:22 -0500

    feat: update astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,5 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind()],
-  experimental: {
-    viewTransitions: true
-  }
+  integrations: [mdx(), sitemap(), tailwind()]
 });
