e22cec6c062c1a577215f628064db19809a8d81a
juanpabotero <juanpabotero1@gmail.com>
2023-09-07 18:03:06 -0500

    fix: define interfaces for components

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'JapyDev | Desarrollador Frontend';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'Portafolio de Juan Pablo Botero, desarrollador frontend.';
